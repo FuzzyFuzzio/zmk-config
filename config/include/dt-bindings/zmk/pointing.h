83856717eea8115d3b4ee7726b3b79d94e321b9a
FuzzyFuzzio <chrisfurmanczyk@gmail.com>
2026-02-20 14:19:32 -0800

    Add stub pointing.h to prevent build errors from GUI keymap editor

---
+++ config/include/dt-bindings/zmk/pointing.h
@@ -0,0 +1,14 @@
+/*
+ * Stub header file for dt-bindings/zmk/pointing.h
+ * 
+ * This file exists to prevent build errors when GUI keymap editors
+ * automatically add: #include <dt-bindings/zmk/pointing.h>
+ * 
+ * If your ZMK build includes native pointing support, this stub will
+ * be overridden by the actual header from ZMK. If not, this empty
+ * stub prevents the "No such file or directory" error.
+ */
+
+#pragma once
+
+/* Stub - actual definitions come from ZMK if pointing module is enabled */
